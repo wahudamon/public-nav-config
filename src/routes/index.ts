d0b6d9f905ab60c140c672f4ac01e7d24c14380a
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2025-02-17 10:45:54 +0700

    feat: initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export const ROOT = '/'
+export const LOGIN = '/login'
+export const FORGOT_PASSWORD = '/forgot-password'
+export const CHANGE_PASSWORD = '/change-password'
+export const DASHBOARD = '/dashboard'
+export const SETTING = '/setting'
+export const NOTIFICATION = '/notification'
